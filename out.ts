626c72c995ae3c34b53d58399e308fbfbacfcaba
Marcin Jabrzyk <marcin.jabrzyk@gmail.com>
2012-04-11 16:29:32 +0200

    The app is now have support for multi-languages. All phrases need to be translated and written to .qm file.

---
+++ out.ts
@@ -0,0 +1,70 @@
+<?xml version="1.0" encoding="utf-8"?>
+<!DOCTYPE TS>
+<TS version="2.0" language="pl_PL">
+<context>
+    <name>MainWindow</name>
+    <message>
+        <location filename="bin/pl/polsl/bd2/ui/MainWindow.jui" line="14"/>
+        <location filename="Generated JUIC files/pl/polsl/bd2/ui/Ui_MainWindow.java" line="64"/>
+        <location filename="src/pl/polsl/bd2/ui/MainWindow.jui" line="14"/>
+        <source>School Helper 2012 Small Demo Edition</source>
+        <translatorcomment>System Wspomagania Szkoły 2012</translatorcomment>
+        <translation>System Wspomagania Szkoły 2012</translation>
+    </message>
+    <message>
+        <location filename="bin/pl/polsl/bd2/ui/MainWindow.jui" line="21"/>
+        <location filename="Generated JUIC files/pl/polsl/bd2/ui/Ui_MainWindow.java" line="65"/>
+        <location filename="src/pl/polsl/bd2/ui/MainWindow.jui" line="21"/>
+        <source>Hello world!</source>
+        <translation>Witaj Świecie!</translation>
+    </message>
+    <message>
+        <location filename="bin/pl/polsl/bd2/ui/MainWindow.jui" line="28"/>
+        <location filename="Generated JUIC files/pl/polsl/bd2/ui/Ui_MainWindow.java" line="66"/>
+        <location filename="src/pl/polsl/bd2/ui/MainWindow.jui" line="28"/>
+        <source>Push to ContactForm</source>
+        <translation>Wiadomości</translation>
+    </message>
+</context>
+<context>
+    <name>contactForm</name>
+    <message>
+        <location filename="bin/pl/polsl/bd2/ui/contactForm.jui" line="14"/>
+        <location filename="Generated JUIC files/pl/polsl/bd2/ui/Ui_contactForm.java" line="80"/>
+        <location filename="src/pl/polsl/bd2/ui/contactForm.jui" line="14"/>
+        <source>Contact Form - Send Message?</source>
+        <translation>Wiadomości - Wysłać wiadomość?</translation>
+    </message>
+    <message>
+        <location filename="bin/pl/polsl/bd2/ui/contactForm.jui" line="27"/>
+        <location filename="Generated JUIC files/pl/polsl/bd2/ui/Ui_contactForm.java" line="81"/>
+        <location filename="src/pl/polsl/bd2/ui/contactForm.jui" line="27"/>
+        <source>Find</source>
+        <translation>Szukaj</translation>
+    </message>
+</context>
+<context>
+    <name>pl.polsl.bd2.gui.MainWindow</name>
+    <message>
+        <location filename="src/pl/polsl/bd2/gui/MainWindow.java" line="25"/>
+        <source>Hi! Push this -&gt; </source>
+        <translation>Kliknij to -&gt;</translation>
+    </message>
+</context>
+<context>
+    <name>pl.polsl.bd2.gui.contactForm</name>
+    <message>
+        <location filename="src/pl/polsl/bd2/gui/contactForm.java" line="46"/>
+        <source>School System Destroyer</source>
+        <comment>Application name</comment>
+        <translatorcomment>System Wspomagania Szkoły 2012</translatorcomment>
+        <translation>System Wspomagania Szkoły 2012</translation>
+    </message>
+    <message>
+        <location filename="src/pl/polsl/bd2/gui/contactForm.java" line="46"/>
+        <source>You have pressed my button</source>
+        <translatorcomment>głupi teskst - głupie tłumaczenie</translatorcomment>
+        <translation>Nacisnąłeś przycisk tworzący mnie</translation>
+    </message>
+</context>
+</TS>
