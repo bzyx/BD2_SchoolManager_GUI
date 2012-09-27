7e67c65491ac5e1aac8b440f2fe2a196ab09ebd0
Mogget <karol.skoruch@gmail.com>
2012-09-27 19:20:57 +0200

    działa

--- src/pl/polsl/bd2/presentation/absence/AbsencePresenter.java
+++ src/pl/polsl/bd2/presentation/absence/AbsencePresenter.java
@@ -2,36 +2,41 @@ package pl.polsl.bd2.presentation.absence;
 
 import com.trolltech.qt.core.QModelIndex;
 
-import pl.polsl.bd2.gui.JustificationForm;
 import pl.polsl.bd2.gui.MainWindow;
 import pl.polsl.bd2.gui.forms.Ui_MainWindow;
 import pl.polsl.bd2.models.AbsenceModel;
 import pl.polsl.bd2.models.JustificationModel;
 import pl.polsl.bd2.presentation.BasePresenter;
 
-
 public class AbsencePresenter implements BasePresenter {
 	private Ui_MainWindow view;
 	private final AbsenceModel absenceModel;
+	private final JustificationModel justificationModel;
+	private MainWindow window;
+
 	public AbsencePresenter(MainWindow window, Ui_MainWindow view) {
 		this.view = view;
 		this.window = window;
 
 		absenceModel = new AbsenceModel();
 		justificationModel = new JustificationModel();
-	}Model = new JustificationModel();
 	}
 
 	@Override
-	@Override
 	public void connectSlots() {
 		view.tableAbsences.doubleClicked.connect(this, AbsenceSlots.ADD_JUSTIFICATION);
+	}
+
 	public void initModel() {
 		view.tableAbsences.setModel(absenceModel);
 		view.listJustifications.setModel(justificationModel);
-	}AbsenceModel getAbsenceModel() {
+	}
+
+	public AbsenceModel getAbsenceModel() {
 		return absenceModel;
-	}sed")
+	}
+
+	@SuppressWarnings("unused")
 	private void addJustification() {
 		if (absenceModel.getActuallAbsenceMock(getCurrentIndex())
 				.getHowMuchAbsence() != 0) {
@@ -41,7 +46,9 @@ public class AbsencePresenter implements BasePresenter {
 			justification.exec();
 			view.listJustifications.reset();
 		}
-	}lIndex getCurrentIndex() {
+	}
+
 	private QModelIndex getCurrentIndex() {
 		return view.tableAbsences.currentIndex();
 	}
+}
