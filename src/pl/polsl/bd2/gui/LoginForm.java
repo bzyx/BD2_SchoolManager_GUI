0b9f8b93b720a0df3f6c3291437dc19c66d8a967
izmajlowiczl <izmajlowiczl@gmail.com>
2012-10-01 14:32:50 +0200

    loginForm -> LoginForm

--- src/pl/polsl/bd2/gui/LoginForm.java
+++ src/pl/polsl/bd2/gui/LoginForm.java
@@ -4,24 +4,24 @@ import pl.polsl.bd2.gui.forms.Ui_loginForm;
 
 import com.trolltech.qt.gui.*;
 
-public class loginForm extends QWidget {
+public class LoginForm extends QWidget {
 
     Ui_loginForm ui = new Ui_loginForm();
 
     public static void main(String[] args) {
         QApplication.initialize(args);
 
-        loginForm testloginForm = new loginForm();
+        LoginForm testloginForm = new LoginForm();
         testloginForm.show();
 
         QApplication.exec();
     }
 
-    public loginForm() {
+    public LoginForm() {
         ui.setupUi(this);
     }
 
-    public loginForm(QWidget parent) {
+    public LoginForm(QWidget parent) {
         super(parent);
         ui.setupUi(this);
     }
